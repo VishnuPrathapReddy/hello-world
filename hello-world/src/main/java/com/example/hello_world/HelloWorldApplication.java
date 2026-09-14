eafbcbbd85ac55fbe872fe5f901f9ae43a6c4b1e
Vishnu Prathap Reddy G <vishnukabaddi010@gmail.com>
2026-09-14 11:16:50 +0530

    created hello api call

--- hello-world/src/main/java/com/example/hello_world/HelloWorldApplication.java
+++ hello-world/src/main/java/com/example/hello_world/HelloWorldApplication.java
@@ -3,14 +3,22 @@ package com.example.hello_world;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@RestController
 public class HelloWorldApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
 
     @Override
     public void run(String... args) {
